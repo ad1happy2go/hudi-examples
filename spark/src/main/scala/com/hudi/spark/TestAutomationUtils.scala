8865bbecda3b3c071fc2de50a9626d30a0ef724f
Rahil Chertara <rchertara@gmail.com>
2025-09-15 12:33:25 -0700

    remove rollback logic similar to 1.0.2 testing

--- spark/src/main/scala/com/hudi/spark/TestAutomationUtils.scala
+++ spark/src/main/scala/com/hudi/spark/TestAutomationUtils.scala
@@ -48,22 +48,22 @@ object TestAutomationUtils {
       val updates = convertToStringList(dataGen.generateUpdates(numUpdates))
       val df = spark.read.json(spark.sparkContext.parallelize(updates, (numUpdates / 100).toInt)).withColumn("batch_id", lit(batch_id)).withColumn("mode", lit(UPDATE_MODE))
       df.write.format("hudi").options(configs).mode("append").save(basePath)
-      val metaClient = HoodieTableMetaClient.builder
-        .setConf(HadoopFSUtils.getStorageConfWithCopy(jsc.hadoopConfiguration))
-        .setBasePath(basePath)
-        .build
-      if (metaClient.getTableConfig.isMetadataTableAvailable && !HoodieTableMetaClient.builder
-        .setConf(HadoopFSUtils.getStorageConfWithCopy(jsc.hadoopConfiguration))
-        .setBasePath(metaClient.getMetaPath.toString + "/metadata")
-        .build.getActiveTimeline.lastInstant().get().getAction.equals(HoodieTimeline.COMMIT_ACTION)) {
-        rollbackLastInstant(spark, basePath, configs)
-        df.write.format("hudi").options(configs).mode("append").save(basePath)
-        df.write.format("parquet").save(basePath + "_parquet_" + batch_id + UPDATE_MODE)
-        assert(spark.read.format("hudi").load(basePath).where(f"batch_id = '${batch_id}'").count() == spark.read.format("parquet").load(basePath + "_parquet_" + batch_id + INSERT_MODE).count())
-      } else {
-        df.write.format("parquet").save(basePath + "_parquet_" + batch_id + UPDATE_MODE)
-        assert(spark.read.format("hudi").load(basePath).where(f"batch_id = '${batch_id}'").count() == spark.read.format("parquet").load(basePath + "_parquet_" + batch_id + INSERT_MODE).count())
-      }
+      //      val metaClient = HoodieTableMetaClient.builder
+      //        .setConf(HadoopFSUtils.getStorageConfWithCopy(jsc.hadoopConfiguration))
+      //        .setBasePath(basePath)
+      //        .build
+      //      if (false && metaClient.getTableConfig.isMetadataTableAvailable && !HoodieTableMetaClient.builder
+      //        .setConf(HadoopFSUtils.getStorageConfWithCopy(jsc.hadoopConfiguration))
+      //        .setBasePath(metaClient.getMetaPath.toString + "/metadata")
+      //        .build.getActiveTimeline.lastInstant().get().getAction.equals(HoodieTimeline.COMMIT_ACTION)) {
+      //        rollbackLastInstant(spark, basePath, configs)
+      //        df.write.format("hudi").options(configs).mode("append").save(basePath)
+      //        df.write.format("parquet").save(basePath + "_parquet_" + batch_id + UPDATE_MODE)
+      //        assert(spark.read.format("hudi").load(basePath).where(f"batch_id = '${batch_id}'").count() == spark.read.format("parquet").load(basePath + "_parquet_" + batch_id + INSERT_MODE).count())
+      //      } else {
+      df.write.format("parquet").save(basePath + "_parquet_" + batch_id + UPDATE_MODE)
+      assert(spark.read.format("hudi").load(basePath).where(f"batch_id = '${batch_id}'").count() == spark.read.format("parquet").load(basePath + "_parquet_" + batch_id + INSERT_MODE).count())
+      //      }
     }
     if (numDeletes > 0) {
       val deletes = convertToStringList(dataGen.generateUpdates(numDeletes))
