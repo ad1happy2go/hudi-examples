8abf918c48ed2c80d4bca0390308c104ff64bfa5
Rahil Chertara <rchertara@gmail.com>
2025-09-17 13:55:26 -0700

    add more profiles

--- spark/scripts/compatibility_test_main.sh
+++ spark/scripts/compatibility_test_main.sh
@@ -28,11 +28,12 @@ formatted_test_version=$(echo "$test_version" | sed 's/\./_/g')
 
 # List of from versions we want to run compatibility tests on
 # versions_to_check=("0.14.1" "0.14.0" "0.15.0")
-versions_to_check=("1.0.2")
+versions_to_check=("0.15.0")
 
 function runCompatibilityTest() {
     local from_version=$1
     local test=$2
+    local run_num=${3:-1}
     local expected_to_version="9"
 
 #    if [[ ${test} == *"noupgrade"* ]]; then
@@ -44,8 +45,8 @@ function runCompatibilityTest() {
     local formatted_from_version=$(echo "$from_version" | sed 's/\./_/g')
     local formatted_to_version=$(echo "$to_version" | sed 's/\./_/g')
 
-    local test_name="${test}_${formatted_from_version}_${formatted_test_version}"
-    echo "Testing ${test} - ${from_version} <> ${test_version}" >> "${result_file}"
+    local test_name="${test}_${formatted_from_version}_${formatted_test_version}_run${run_num}"
+    echo "Testing ${test} - ${from_version} <> ${test_version} (Run ${run_num})" >> "${result_file}"
     sh compatibility_test.sh -j "${test_jar}" -tv "${test_version}" -fv "${from_version}" -c rahil_configs/${test}.props -etv ${expected_to_version} > "logs/${test_name}.log" 2>&1
     local exit_code=$?
     # Enable this if we want to run long running tests
@@ -55,20 +56,29 @@ function runCompatibilityTest() {
 
 # Create properties file for each test case. The name of properties file should end with .props
 for from_version in "${versions_to_check[@]}"; do
-     runCompatibilityTest "${from_version}" "basic_cow"
-     runCompatibilityTest "${from_version}" "basic_mor"
-     runCompatibilityTest "${from_version}" "cow_partitioned"
-     runCompatibilityTest "${from_version}" "cow_partitioned_defaultPayload"
-#    runCompatibilityTest "${from_version}" "cow_partitioned_metadata_disabled"
-     runCompatibilityTest "${from_version}" "cow_partitioned_metadata_enabled"
-#    runCompatibilityTest "${from_version}" "cow_partitioned_metadata_enabled_recordIndexEnabled"
-     runCompatibilityTest "${from_version}" "cow_partitioned_metadata_enabled_simpleIndexEnabled"
-     runCompatibilityTest "${from_version}" "cow_partitioned_overwritePayload"
-     runCompatibilityTest "${from_version}" "mor_partitioned"
-     runCompatibilityTest "${from_version}" "mor_partitioned_defaultPayload"
-#    runCompatibilityTest "${from_version}" "mor_partitioned_metadata_disabled"
-     runCompatibilityTest "${from_version}" "mor_partitioned_metadata_enabled"
-#    runCompatibilityTest "${from_version}" "mor_partitioned_metadata_enabled_recordIndexEnabled"
-     runCompatibilityTest "${from_version}" "mor_partitioned_metadata_enabled_simpleIndexEnabled"
-     runCompatibilityTest "${from_version}" "mor_partitioned_overwritePayload"
+    echo "Starting 1 runs for version ${from_version}..." >> "${result_file}"
+    for run in {1..1}; do
+        echo "=== Run ${run} of 1 ===" >> "${result_file}"
+        #runCompatibilityTest "${from_version}" "basic_cow" "${run}"
+        #runCompatibilityTest "${from_version}" "basic_mor" "${run}"
+#       runCompatibilityTest "${from_version}" "cow_partitioned" "${run}"
+#       runCompatibilityTest "${from_version}" "cow_partitioned_defaultPayload" "${run}"
+        runCompatibilityTest "${from_version}" "cow_partitioned_metadata_disabled" "${run}"
+#       runCompatibilityTest "${from_version}" "cow_partitioned_metadata_enabled" "${run}"
+        runCompatibilityTest "${from_version}" "cow_partitioned_metadata_enabled_recordIndexEnabled" "${run}"
+#       runCompatibilityTest "${from_version}" "cow_partitioned_metadata_enabled_simpleIndexEnabled" "${run}"
+        runCompatibilityTest "${from_version}" "cow_partitioned_metadata_enabled_bloomIndexEnabled" "${run}"
+        runCompatibilityTest "${from_version}" "cow_partitioned_OverwriteNonDefaultsWithLatestAvroPayload" "${run}"
+#       runCompatibilityTest "${from_version}" "cow_partitioned_overwritePayload" "${run}"
+#       runCompatibilityTest "${from_version}" "mor_partitioned" "${run}"
+#       runCompatibilityTest "${from_version}" "mor_partitioned_defaultPayload" "${run}"
+        runCompatibilityTest "${from_version}" "mor_partitioned_metadata_disabled" "${run}"
+#       runCompatibilityTest "${from_version}" "mor_partitioned_metadata_enabled" "${run}"
+        runCompatibilityTest "${from_version}" "mor_partitioned_metadata_enabled_recordIndexEnabled" "${run}"
+#       runCompatibilityTest "${from_version}" "mor_partitioned_metadata_enabled_simpleIndexEnabled" "${run}"
+        runCompatibilityTest "${from_version}" "mor_partitioned_metadata_enabled_bloomIndexEnabled" "${run}"
+        runCompatibilityTest "${from_version}" "mor_partitioned_OverwriteNonDefaultsWithLatestAvroPayload" "${run}"
+#       runCompatibilityTest "${from_version}" "mor_partitioned_overwritePayload" "${run}"
+    done
+    echo "Completed 1 runs for version ${from_version}" >> "${result_file}"
 done
